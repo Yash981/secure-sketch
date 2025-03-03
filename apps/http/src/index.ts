ee9428055d208d441e22a8c2583798aafd104bf7
Yashwanth <dyashwanth14011999@gmail.com>
2025-03-03 13:00:32 +0530

    feat(websocket): refactor WebSocket server to integrate with HTTP server and improve configuration

--- apps/http/src/index.ts
+++ apps/http/src/index.ts
@@ -1,11 +1,14 @@
 import express from "express";
 import UserRouter from "./routes/v1/user-router";
 import cookieParser from "cookie-parser";
+import http from 'http'
 import dotenv from "dotenv";
 import cors from 'cors'
-dotenv.config({path: "../../.env"});
-const app =  express()
+import { setupWebSocketServer} from '@repo/ws'
 
+dotenv.config({path: "../../.env"});
+const PORT = process.env.PORT || 9000
+const app = express()
 app.use(express.json())
 app.use(express.raw({ type: "application/octet-stream", limit: "10mb" }));
 app.use(cookieParser())
@@ -14,8 +17,16 @@ app.use(cors({
     credentials:true
   }))
 app.use('/api/v1',UserRouter) 
+const server = http.createServer(app);
+setupWebSocketServer(server);
 
-const PORT = process.env.PORT || 9000
-app.listen(PORT,()=>{
-    console.log('Server is running on http://localhost:9000')
+server.listen(PORT,()=>{
+    console.log(`Server is running on http://localhost:${PORT}`)
 })
+process.on("SIGINT", () => {
+  console.log("Shutting down server...");
+  server.close(() => {
+    console.log("HTTP server closed.");
+    process.exit(0);
+  });
+});
--- apps/ws/src/websocket-server.ts
+++ apps/ws/src/websocket-server.ts
@@ -1,35 +1,41 @@
 import { WebSocket, WebSocketServer } from "ws";
 import { VerifyUser } from "@repo/backend-common";
 import { CollaborationManager, User } from "./managers/collaboration-manager";
-const PORT = Number(process.env.PORT) || 8080; 
-const wss = new WebSocketServer({ port: PORT });
-console.log('WebSocket server starting on ws://localhost:8080'); 
-const collobrationManager = new CollaborationManager();
-wss.on('listening', () => {
-  console.log('WebSocket server is now running');
-});
-wss.on("connection", function connection(ws: WebSocket, req) {
-  const url = req.url;
-  if (!url) {
-    return;
-  }
+export const setupWebSocketServer = (server:any) => {
 
-  const queryParams = new URLSearchParams(url.split("?")[1]);
-  const token = queryParams.get("token") || "";
-  const checkUserExist = VerifyUser(token);
-  if (checkUserExist == null) {
-    ws.close();
-    return null;
-  }
-  const user = new User(ws, checkUserExist, checkUserExist.split('@')[0] as string);
-  collobrationManager.addUser(user)
-  ws.on("close", (event) => {
-    console.log(event, "event");
-    console.log("Client disconnected");
-    collobrationManager.broadcastToRoom(JSON.stringify({ type: "USER_LEFT", payload: { userId: user.email } }), user.email);
-    collobrationManager.removeUser(user.email)
+  const wss = new WebSocketServer({ server });
+
+  console.log('WebSocket server attached to HTTP server'); 
+  const collobrationManager = new CollaborationManager();
+
+  wss.on('listening', () => {
+    console.log('WebSocket server is now running');
   });
-  ws.on("error", (err) => {
-    console.error(err);
+
+  wss.on("connection", function connection(ws: WebSocket, req) {
+    const url = req.url;
+    if (!url) {
+      return;
+    }
+
+    const queryParams = new URLSearchParams(url.split("?")[1]);
+    const token = queryParams.get("token") || "";
+    const checkUserExist = VerifyUser(token);
+    if (checkUserExist == null) {
+      ws.close();
+      return null;
+    }
+    const user = new User(ws, checkUserExist, checkUserExist.split('@')[0] as string);
+    collobrationManager.addUser(user)
+    ws.on("close", (event) => {
+      console.log(event, "event");
+      console.log("Client disconnected");
+      collobrationManager.broadcastToRoom(JSON.stringify({ type: "USER_LEFT", payload: { userId: user.email } }), user.email);
+      collobrationManager.removeUser(user.email)
+    });
+    ws.on("error", (err) => {
+      console.error(err);
+      ws.send(JSON.stringify({err}))
+    });
   });
-});
+}
