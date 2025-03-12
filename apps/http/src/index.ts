a2cc897bddca7e6cba686001abd0a68d26ac2275
Yashwanth <dyashwanth14011999@gmail.com>
2025-03-12 14:21:25 +0530

    feat(cors): enhance CORS configuration with allowed headers and methods

--- apps/http/src/index.ts
+++ apps/http/src/index.ts
@@ -2,20 +2,24 @@ import express from "express";
 import UserRouter from "./routes/v1/user-router";
 import cookieParser from "cookie-parser";
 import dotenv from "dotenv";
-import cors from 'cors'
+import cors from "cors";
 
-dotenv.config({path: "../../.env"});
-const PORT = process.env.PORT || 9000
-const app = express()
-app.use(express.json())
+dotenv.config({ path: "../../.env" });
+const PORT = process.env.PORT || 9000;
+const app = express();
+app.use(express.json());
 app.use(express.raw({ type: "application/octet-stream", limit: "10mb" }));
-app.use(cookieParser())
-app.use(cors({
+app.use(cookieParser());
+app.use(
+  cors({
     origin: process.env.FRONTEND_URL,
-    credentials:true
-  }))
-app.use('/api/v1',UserRouter) 
+    credentials: true,
+    allowedHeaders: ["Content-Type", "Authorization"],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+  })
+);
+app.use("/api/v1", UserRouter);
 
-app.listen(PORT,()=>{
-    console.log(`Server is running on http://localhost:${PORT}`)
-})
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
